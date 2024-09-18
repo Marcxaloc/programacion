5a5f223634ebe96794eb9576dbebcf637ef54b64
Marcxaloc <marc.estevez@campus.xaloc.org>
2024-09-18 12:58:08 +0200

    Add files via upload

---
+++ programacion/python/ejemplos/usuario valor.py
@@ -0,0 +1,36 @@
+#imput: es para que el usuario pueda crear sus propias variables y responder preguntas
+#int: combierte el texto en numeros enteros
+#float: en cambio del int es para usar numeros decimales
+#str: se utiliza para pasra de numeros a letras 
+
+var1=int(input("introduce el primer numero para la suma:"))
+
+var2=int(input("introduce el segundo numero para la suma:"))
+
+total=var1+var2
+
+print ("la suma es:",total)
+
+var3=int(input("introduce el primer numero para la resta:"))
+
+var4=int(input("introduce el segundo numero para la resta:"))
+
+total2=var3-var4
+
+print ("la resta es:",total2)
+
+var5=int(input("introduce el primer numero para la multipilicacion:"))
+
+var6=int(input("introduce el segundo numero para la multipilcacion:"))
+
+total3=var5*var6
+
+print ("la multiplicacion es:",total3)
+
+var7=int(input("introduce el primer numero para la division:"))
+
+var8=int(input("introduce el segundo numero parala division:"))
+
+total4=var7/var8
+
+print ("la division es:",total4)
