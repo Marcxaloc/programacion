f5363e14255d52f910007e670fe46b0e3e1004f5
Marcxaloc <marc.estevez@campus.xaloc.org>
2024-09-30 19:31:26 +0200

    Add files via upload

---
+++ programacion/ejercicios_iniciales/ejercicio 11.py
@@ -0,0 +1,5 @@
+var1=int(input("dime un numero: "))
+area=var1*var1
+perimetro=var1+var1+var1+var1
+print("el area es: ",area)
+print("el perimetro es: ",perimetro)
---
+++ programacion/ejercicios_iniciales/ejercicio 8.py
@@ -0,0 +1,4 @@
+hora1= int(input("dime una hora "))
+print ("En minutos son ",hora1*60)
+
+print("en segundos son ",hora1*3600)
